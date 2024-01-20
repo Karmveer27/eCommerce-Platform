4a80229886b66c05121701540983330b2bb2a90c
Karmveer <karmveer.nz@gmail.com>
2024-01-20 22:21:56 +1300

    Created Header componenent

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { Navbar, Nav, Container} from 'react-bootstrap'
+import { FaShoppingCart, FaUser} from 'react-icons/fa'
+
+function Header() {
+  return (
+    <header>
+        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>  {/* collapseOnSelect enabled means when a user selects an option, it closes the navbar. The expand sets the breakpoint for collapse/not collapsed */}
+    
+            <Container>
+                <Navbar.Brand href="#"> Proshop </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className="ms-auto">
+
+                        <Nav.Link href='/cart'>
+                            <FaShoppingCart /> Cart
+                        </Nav.Link>
+                        <Nav.Link href='/login'>
+                           <FaUser /> Login
+                        </Nav.Link>
+                        
+                    </Nav>
+                </Navbar.Collapse>
+
+
+            </Container>
+            
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
