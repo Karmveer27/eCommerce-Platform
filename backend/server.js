fdec380325eb11f1bb9f684646652a71cc0d1646
Karmveer <karmveer.nz@gmail.com>
2024-01-26 20:28:22 +1300

    Added ./product/:id route to access a single product

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ app.get('/', (req,res) => {
     res.send('API is Runiing')
 });
 
+app.get('/products', (req,res) => {
+    res.json(products); // res.send sends a string response insead
+})
+
+app.get('/products/:id',(req,res) => {
+    const id = req.params.id;
+    const product = products.find((p) => p._id === id);
+    res.json(product);
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 }); //Start listening on the port we declared above
